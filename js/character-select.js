f763670d7386f62236540ffa1d98e8212853f3f9
Makaveli <147116214+NicholasMakaveli@users.noreply.github.com>
2024-09-06 14:46:25 -0400

    Merge again

--- js/character-select.js
+++ js/character-select.js
@@ -34,7 +34,14 @@ for (let i = startFrame; i < startFrame + totalFrames; i++)
 
 
 
-
+//Mouse over event for image spirtes
+//Trying to have sprites show up on mouse over
+let Character = document.querySelector('Character');
+Character.forEach(element =>{
+    addEventListener("mouseover", (event) => {
+        
+    });
+});
 
 // Event listener for triggering Jin's idle animation
 document.querySelector('#JinSelect').addEventListener('click', jinSelectIdle);
