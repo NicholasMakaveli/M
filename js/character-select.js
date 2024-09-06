6f616f2756d3f4040f2cc30684a56ad8713ac0f8
Eddie Baumann <15ebaumann@gmail.com>
2024-09-06 14:35:29 -0400

    Working towards mouse over event

--- js/character-select.js
+++ js/character-select.js
@@ -10,3 +10,11 @@ jinSpriteSheet.src = 'assets/JinIdle.png';
 // Event listener for triggering Jin's idle animation
 document.querySelector('#JinSelect').addEventListener('click', jinSelectIdle);
 
+//Mouse over event for image spirtes
+//Trying to have sprites show up on mouse over
+let Character = document.querySelector('Character');
+Character.forEach(element =>{
+    addEventListener("mouseover", (event) => {
+        
+    });
+});
